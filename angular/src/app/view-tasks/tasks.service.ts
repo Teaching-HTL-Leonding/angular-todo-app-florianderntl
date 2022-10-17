0b8efd8627df20cb344121f5f30771394dface57
Florian Derntl <f.derntl@students.htl-leonding.ac.at>
2022-10-17 18:44:15 +0200

    Remove unnecessary code comments

--- angular/src/app/view-tasks/tasks.service.ts
+++ angular/src/app/view-tasks/tasks.service.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 export interface Task {
   description: string,
   assignedPerson: string,
-  done: boolean
-  // constructor(public description: string = "", public assignedPerson: string = "", public done: boolean = false) { }
+  done: boolean,
 }
 
 @Injectable({
@@ -12,7 +11,6 @@ export interface Task {
 })
 export class TasksService {
   public tasks: Task[];
-  // public tasks!: Task[];  // used for filtering
 
   constructor() {
     this.tasks = [
@@ -20,33 +18,7 @@ export class TasksService {
       { description: "Task2", assignedPerson: "Erik", done: true },
       { description: "Task2", assignedPerson: "", done: false },
     ];
-
-    // this.resetFilter();
   }
-
-  // public resetFilter() {
-  //   this.tasks = this._tasks;
-  // }
-
-  // public filterByPerson(name: string) {
-  //   let tmpTasks = [];
-  //   for (let task of this.tasks) {
-  //     if (task.assignedPerson === name) {
-  //       tmpTasks.push(task);
-  //     }
-  //   }
-  //   this.tasks = tmpTasks;
-  // }
-
-  // public filterByDone(showDoneTasks: boolean) {
-  //   let tmpTasks = [];
-  //   for (let task of this.tasks) {
-  //     if (!task.done || task.done === showDoneTasks) {
-  //       tmpTasks.push(task);
-  //     }
-  //   }
-  //   this.tasks = tmpTasks;
-  // }
 
   public deleteTask(taskIx: number) {
     this.tasks.splice(taskIx, 1);
@@ -54,6 +26,5 @@ export class TasksService {
 
   public addEmptyTask() {
     this.tasks.unshift({ description: "", assignedPerson: "", done: false });
-    // this.resetFilter();
   }
 }
